5aa0c98fe91d689e0ac8dbc7a7f17e1ac2dbf43e
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-03-20 11:27:24 +0700

    Update SnoopCommand.cs

--- CADPythonShell/SnoopCommand.cs
+++ CADPythonShell/SnoopCommand.cs
@@ -1,13 +1,4 @@
-﻿using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Runtime;
-using CADSnoop.Model;
-using CADSnoop.View;
-using CADSnoop.ViewModel;
-using Exception = System.Exception;
-using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
-using MessageBox = System.Windows.MessageBox;
+﻿using Autodesk.AutoCAD.Runtime;
 
 namespace CADPythonShell
 {
@@ -16,51 +7,7 @@ namespace CADPythonShell
         [CommandMethod("Snoop")]
         public void Execute()
         {
-            try
-            {
-                Document doc = Application.DocumentManager.MdiActiveDocument;
-                Editor ed = doc.Editor;
-                Database db = doc.Database;
-                using (Transaction tran = db.TransactionManager.StartTransaction())
-                {
-                    List<ObjectId> objectIds = PickObjectBySelect(doc);
-                    if (objectIds!=null)
-                    {
-                        SnoopViewModel vm = new SnoopViewModel(doc, db, objectIds);
-                        MainWindow form = new MainWindow(vm);
-                        form.SetCadAsWindowOwner();
-                        form.Show();
-                    }
-                    tran.Commit();
-
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
+           new CADSnoop.SnoopCommand().Execute();
         }
-
-        List<ObjectId> PickObjectBySelect(Document doc)
-        {
-            try
-            {
-                //PromptSelectionOptions poOptions = new PromptSelectionOptions();
-                //poOptions.SingleOnly = true;
-                PromptSelectionResult promptSelectionResult = doc.Editor.GetSelection();
-                if (promptSelectionResult.Status != PromptStatus.OK) return null;
-                SelectionSet selectionSet = promptSelectionResult.Value;
-                return selectionSet.GetObjectIds().ToList();
-            }
-            catch(ArgumentNullException){}
-            catch(NullReferenceException){}
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString());
-            }
-
-            return null;
-        }
-
     }
 }
