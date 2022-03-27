0b23a0f2350195c30d902f6a286c6ea6d8c8771c
chuongmep <chuongpqvn@gmail.com>
2022-03-27 16:38:29 +0700

    Create showAlertDialog.py

---
+++ Script Examples/showAlertDialog.py
@@ -0,0 +1,21 @@
+#Copyright(c) 2021, Hồ Văn Chương
+# @chuongmep, https://chuongmep.com/
+import clr
+import sys
+sys.path.append('C:\Program Files (x86)\IronPython 2.7\Lib')
+import os
+import math
+clr.AddReference('acmgd')
+clr.AddReference('acdbmgd')
+clr.AddReference('accoremgd')
+# Import references from AutoCAD
+from Autodesk.AutoCAD.Runtime import *
+from Autodesk.AutoCAD.ApplicationServices import *
+from Autodesk.AutoCAD.EditorInput import *
+from Autodesk.AutoCAD.DatabaseServices import *
+from Autodesk.AutoCAD.Geometry import *
+doc = Application.DocumentManager.MdiActiveDocument
+ed = doc.Editor
+db = doc.Database
+# Write Code Below
+Application.ShowAlertDialog("An error occurred!")
